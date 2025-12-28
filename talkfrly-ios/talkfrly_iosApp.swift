4e6d6dfffb4584feda9a0b3a2bb93f97ba4417b1
michaelarchman <mike.projektowanie@gmail.com>
2025-12-28 22:58:59 +0100

    Initial Commit

---
+++ talkfrly-ios/talkfrly_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  talkfrly_iosApp.swift
+//  talkfrly-ios
+//
+//  Created by Mike on 28/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct talkfrly_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
